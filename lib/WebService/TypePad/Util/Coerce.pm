97914bdb08a57c54231572a0bba9088abc6fe216
Martin Atkins <mart@degeneration.co.uk>
2009-12-30 12:36:47 -0800

    Add the empty placeholder for the "Coerce" utility package that the object type implementations use to coerce between API and Perl representations.

---
+++ lib/WebService/TypePad/Util/Coerce.pm
@@ -0,0 +1,17 @@
+
+=head1 NAME
+
+WebService::TypePad::Util::Coerce - Utility functions used for coercing values into and out of TypePad::API::Object subclasses
+
+=cut
+
+package WebService::TypePad::Util::Coerce;
+
+use strict;
+use warnings;
+
+
+
+1;
+
+
